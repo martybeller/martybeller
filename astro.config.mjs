69d33d2ac4f86d8bcc7ac2079edcce8b54702a62
Robert C. Bowman III <rbowman@hanekedesign.com>
2026-03-17 16:09:14 -0400

    update root domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
-
 // https://astro.build/config
 export default defineConfig({
-  site: isGitHubPages
-    ? 'https://martybeller.github.io/martybeller'
-    : 'http://localhost:4321',
-  base: isGitHubPages ? '/martybeller' : '/',
+  site: 'https://martybeller.com',
   vite: {
     plugins: [tailwindcss()]
   }
